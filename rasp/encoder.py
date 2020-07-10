af289741559f03b77f826649d0bde93ac33ced50
Renan Starke <xtarke@gmail.com>
2020-07-10 10:19:07 -0300

    Tested rpm

--- rasp/encoder.py
+++ rasp/encoder.py
@@ -3,10 +3,15 @@ import time
 
 # http://abyz.me.uk/rpi/pigpio/
 
+wheel_pulses = 20
+counter = 0
+
 def cbf(gpio, level, tick):
-    print(gpio, level, tick)
+    global counter
+    counter = counter + 1
 
 def main():
+    global counter
 
     pi = pigpio.pi()
     
@@ -17,9 +22,13 @@ def main():
     
     while True:
         
-        
-        
         time.sleep(1)
+        
+        # rpm = (60 * 1000 / wheel_pulses ) / (millis() - timeold) * counter;
+        rpm = (60 * 1000 / wheel_pulses ) / 1000 * counter;
+        counter = 0;
+        
+        print(rpm)
     
 
 if __name__ == '__main__':
